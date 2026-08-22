c122812606f448a25d4168dc570cecf9b0a40140
Ankit Kulria <kulriaankit25@gmail.com>
2026-08-22 22:02:44 +0530

    Selection sort

---
+++ src/arrays/sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+package arrays.sorting;
+
+public class SelectionSort {
+    static void main() {
+        int[] arr={4,3,2,5,6,1,7};
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print("  "+arr[i]);
+        }
+    }
+}
